ef403eac000e6aaf2463fb0fa351db1629523c51
Sara Gunnars <gunnars.sara@gmail.com>
2022-10-05 09:03:11 +0200

    code along

--- Code-alongs/Lec15-modules/example3_paths/main.py
+++ Code-alongs/Lec15-modules/example3_paths/main.py
@@ -4,4 +4,16 @@ rootpath = os.path.dirname(__file__)
 path_folder1 = os.path.join(rootpath, "folder1")
 path_folder2 = os.path.join(rootpath, "folder2")
 
+
+os.system("cls||clear")
+print("="*100)
+
+print (rootpath)
 print(path_folder1)
+
+print("="*100)
+
+sys.path.append(path_folder1)
+sys.path.append(path_folder2)
+
+import module1, module2
